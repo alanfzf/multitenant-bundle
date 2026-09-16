d7d1805e5760c054a031c01feb41b88f4a333dfa
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-09-16 15:47:10 -0600

    feat(bundle): bundle bootstrapping

--- src/AlanfzfMultiTenantBundle.php
+++ src/AlanfzfMultiTenantBundle.php
@@ -3,10 +3,13 @@
 namespace Alanfzf\MultitenatBundle;
 
 use Alanfzf\MultitenatBundle\Doctrine\DBAL\TenantConnection;
+use Alanfzf\MultitenatBundle\Doctrine\ORM\TenantEntityManager;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 class AlanfzfMultiTenantBundle extends AbstractBundle
 {
@@ -14,6 +17,17 @@ class AlanfzfMultiTenantBundle extends AbstractBundle
     {
         $definition->rootNode()
         ->children()
+
+            // tenant connection configuration
+            ->arrayNode('tenant_connection')
+                ->addDefaultsIfNotSet()
+                ->children()
+                    ->scalarNode('url')
+                        ->defaultValue('%env(DATABASE_URL)%')
+                    ->end()
+                ->end()
+            ->end()
+            // tenant migration configuration
            ->arrayNode('tenant_migration')
                ->addDefaultsIfNotSet()
                ->children()
@@ -26,6 +40,7 @@ class AlanfzfMultiTenantBundle extends AbstractBundle
                ->end()
            ->end()
 
+            // tenant entity manager configuration
            ->arrayNode('tenant_entity_manager')
                ->addDefaultsIfNotSet()
                ->children()
@@ -59,33 +74,48 @@ class AlanfzfMultiTenantBundle extends AbstractBundle
         ContainerConfigurator $container,
         ContainerBuilder $builder,
     ): void {
+
+        // Load the Doctrine configuration for the tenant connection and entity manager
         $builder->prependExtensionConfig('doctrine', [
             'dbal' => [
                 'connections' => [
                     'tenant' => [
-                        'url' => '%env(resolve:DATABASE_URL)%',
+                        'url' => $config['tenant_connection']['url'],
                         'wrapper_class' => TenantConnection::class,
                     ],
                 ],
             ],
+
             'orm' => [
                 'entity_managers' => [
                     'tenant' => [
                         'connection' => 'tenant',
-                        'naming_strategy' => 'doctrine.orm.naming_strategy.underscore_number_aware',
+                        'naming_strategy' => $config['tenant_entity_manager']['tenant_naming_strategy'],
                         'mappings' => [
                             'AlanfzfMultiTenantBundle' => [
-                                // use php 8 attributes for mappings
                                 'type' => 'attribute',
-                                'dir' => '%kernel.project_dir%/src/Entity/Tenant',
-                                'prefix' => 'App\Entity\Tenant',
-                                'alias' => 'Tenant',
-                                'is_bundle' => false,
+                                'dir' => $config['tenant_entity_manager']['mapping']['dir'],
+                                'prefix' => $config['tenant_entity_manager']['mapping']['prefix'],
+                                'alias' => $config['tenant_entity_manager']['mapping']['alias'],
+                                'is_bundle' => $config['tenant_entity_manager']['mapping']['is_bundle'],
                             ],
                         ],
                     ],
                 ],
             ],
         ]);
+
+
+        $container->services()
+        ->set('tenant_entity_manager', TenantEntityManager::class)
+        ->args([
+            service('doctrine.orm.tenant_entity_manager'),
+        ])
+        ->public();
+
+        $container->alias(
+            TenantEntityManager::class,
+            'tenant_entity_manager',
+        );
     }
 }
